88aeba76d7a044c785172cb4cfc55c82c74871bd
ZiqiXiao <99584387+ZiqiXiao@users.noreply.github.com>
2023-10-22 18:19:28 +0800

    Add logger from setting.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from config.settings import app_logger as logger
+
 app = FastAPI()
 
 
 @app.get("/hello-world")
 def hello_world() -> dict[str, str]:
+    logger.info("hello world")
     return {"message": "Hello World!"}
