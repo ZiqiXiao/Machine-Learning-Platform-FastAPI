d1e22ba1cda6c5a1067b995aa2a9ba460358262d
ZiqiXiao <99584387+ZiqiXiao@users.noreply.github.com>
2023-10-22 23:53:57 +0800

    Bug fix: extra type and filter

--- config/settings.py
+++ config/settings.py
@@ -15,25 +15,25 @@ logger.level("SUCCESS", color="<fg #00FF00><bold>", icon="🎉")
 # TODO: ICON is not shown in console
 logger.add(
     std,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> {extra['type]} | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> {extra[type]} | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     colorize=True,
     level="DEBUG",
-    filter=lambda record: record["extra"].get("type") == "app",
     # enqueue=True,
 )
 logger.add(
     "./logs/app_dev.log",
-    format="{time:YYYY-MM-DD HH:mm:ss.SSS} {extra['type']} | {level: <8} | {name}:{function}:{line} - {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss.SSS} {extra[type]} | {level: <8} | {name}:{function}:{line} - {message}",
     rotation="1 day",
     retention="10 days",
     level="DEBUG",
+    filter=lambda record: record["extra"].get("type") == "app",
     # enqueue=True,
 )
 app_logger = logger.bind(type="app")
 
 logger.add(
     "./ml/logs/ml_dev.log",
-    format="{time:YYYY-MM-DD HH:mm:ss.SSS} {extra['type']} | {level: <8} | {name}:{function}:{line} - {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss.SSS} {extra[type]} | {level: <8} | {name}:{function}:{line} - {message}",
     rotation="1 day",
     retention="10 days",
     level="DEBUG",
