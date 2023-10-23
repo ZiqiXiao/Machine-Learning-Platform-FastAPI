ac90eab9719b61d2bd25957e1b709a271b9ff74d
ZiqiXiao <99584387+ZiqiXiao@users.noreply.github.com>
2023-10-23 21:15:23 +0800

    A functional version. Now can train, predict, save.

--- ml/linear/LinearRegression.py
+++ ml/linear/LinearRegression.py
@@ -1,15 +1,28 @@
+import os
+from logging import Logger
+from uuid import UUID
+
+import joblib
+import loguru
+import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+from app.models import ml
 from config.settings import ml_logger as logger
 from ml.linear import LinearBM
 
 
+# TODO: Add WebSocket support
+# TODO: Unique ID for each request
 class LinearRegressionModel(LinearBM.LinearBaseModel):
     def __init__(self):
-        self.model = LinearRegression()
-        self.logger = logger
+        super().__init__()
+        self.model: LinearRegression = LinearRegression()
+        self.logger: Logger = logger
 
-    @logger.catch(reraise=True)
+    # TODO: Metrics
+    @loguru.logger.catch(reraise=True)
     def train(self, train_params: LinearBM.TrainParams):
         self.logger.info("Start training")
         X_train, X_test, y_train, y_test = self._prepare_data(
@@ -20,11 +33,25 @@ class LinearRegressionModel(LinearBM.LinearBaseModel):
         self.model.fit(X_train, y_train)
         self.logger.success("Training finished")
 
-    def predict(self, data):
-        pass
+    @loguru.logger.catch(reraise=True)
+    def predict(self, pred_params: LinearBM.PredictParams) -> np.ndarray:
+        self.logger.info("Start predicting")
+        self.model: LinearRegression = joblib.load(pred_params.path_to_model)
+        self.logger.info(f"Model loaded from {pred_params.path_to_model}")
+        df: pd.DataFrame = pd.read_csv(pred_params.data_path)
+        predicted = self.model.predict(df)
+        self.logger.success("Predicting finished")
+        return predicted
 
-    def save(self, path):
-        pass
+    @loguru.logger.catch(reraise=True)
+    def save(self) -> ml.ModelInfo:
+        os.makedirs(self._train_store_path, exist_ok=True)
+        save_path: str = os.path.join(self._train_store_path, f"model.joblib")
+        joblib.dump(self.model, save_path)
+        self.logger.info(f"Model saved {save_path}")
 
-    def load(self, path):
-        pass
+        model_save: ml.ModelInfo = ml.ModelInfo(
+            uid=str(self._uid),
+            path=save_path,
+        )
+        return model_save
